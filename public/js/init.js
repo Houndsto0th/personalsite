e7816eaa585fcb27a61f593ceaa9ee40e09e216b
Houndsto0th <bry.kinnear@gmail.com>
2015-02-17 14:00:34 -0700

    more changes to portfolio

--- public/js/init.js
+++ public/js/init.js
@@ -5,36 +5,36 @@ $(document).ready(function(){
 
 $('.btn-ink').hover(function () {
   $(this).toggleClass('picture');
-  $(this).toggleClass('plus');
+  $(this).toggleClass('link');
 
 });
 
-$('.btn-ink').on('click', function () {
-  var $this = $(this);
-  var $window = $(window);
-  $this.toggleClass('centered');
-  if ($this.hasClass('centered')) {
-  $this.toggleClass('modalactive');
-    $this.velocity({
-        scaleX: 10,
-        scaleY: 10,
-        borderRadius: "0",
-        top: ((($window.height() - $this.outerHeight()) / 2) + $window.scrollTop() + "px"),
-        left: ((($window.width() - $this.outerWidth()) / 2) + $window.scrollLeft() + "px"),
-        zIndex: 1,
-        backgroundColor: '#ffffff',
-
-      }, 50 );
-  } else {
-
-    $this.velocity("reverse");
-    $this.toggleClass('modalactive');
-
-  }
-  $this.children().toggleClass('hidden');
-  $this.find('span').toggle();
-
-});
+// $('.btn-ink').on('click', function () {
+//   var $this = $(this);
+//   var $window = $(window);
+//   $this.toggleClass('centered');
+//   if ($this.hasClass('centered')) {
+//   $this.toggleClass('modalactive');
+//     $this.velocity({
+//         scaleX: 10,
+//         scaleY: 10,
+//         borderRadius: "0",
+//         top: ((($window.height() - $this.outerHeight()) / 2) + $window.scrollTop() + "px"),
+//         left: ((($window.width() - $this.outerWidth()) / 2) + $window.scrollLeft() + "px"),
+//         zIndex: 1,
+//         backgroundColor: '#ffffff',
+//
+//       }, 50 );
+//   } else {
+//
+//     $this.velocity("reverse");
+//     $this.toggleClass('modalactive');
+//
+//   }
+//   $this.children().toggleClass('hidden');
+//   $this.find('span').toggle();
+//
+// });
 
 $('.social').on('click', function () {
   $(this).toggleClass('octicon-mark-github');
